2a700e1ef6533629b4bafd5217853ae3b9664c4d
snoz-an <snoz.andrey94@mail.ru>
2023-11-07 21:02:54 +0300

    fix

--- index.php
+++ index.php
@@ -4,99 +4,99 @@
 $data = json_decode(file_get_contents('php://input'), TRUE);
 //file_put_contents('file.txt', '$data: '.print_r($data, 1)."\n", FILE_APPEND);
 
-
+//https://github.com/snoz-an/mybot/index.php
 //https://api.telegram.org/bot6915062776:AAEDGQ4EJ56SKgjlrvvKR_P665gVunHpWww/setwebhook?url=https://github.com/snoz-an/mybot/index.php
 
 
 # Обрабатываем ручной ввод или нажатие на кнопку
-$data = $data['callback_query'] ? $data['callback_query'] : $data['message'];
+// $data = $data['callback_query'] ? $data['callback_query'] : $data['message'];
 
-# Важные константы
-define('TOKEN', '6915062776:AAEDGQ4EJ56SKgjlrvvKR_P665gVunHpWww');
+// # Важные константы
+// define('TOKEN', '6915062776:AAEDGQ4EJ56SKgjlrvvKR_P665gVunHpWww');
 
-# Записываем сообщение пользователя
-$message = mb_strtolower(($data['text'] ? $data['text'] : $data['data']),'utf-8');
-
-
-# Обрабатываем сообщение
-switch ($message)
-{
-    case 'текст':
-        $method = 'sendMessage';
-        $send_data = [
-            'text'   => 'Вот мой ответ'
-        ];
-        break;
-
-    case 'кнопки':
-        $method = 'sendMessage';
-        $send_data = [
-            'text'   => 'Вот мои кнопки',
-            'reply_markup' => [
-                'resize_keyboard' => true,
-                'keyboard' => [
-                    [
-                        ['text' => 'Видео'],
-                        ['text' => 'Кнопка 2'],
-                    ],
-                    [
-                        ['text' => 'Кнопка 3'],
-                        ['text' => 'Кнопка 4'],
-                    ]
-                ]
-            ]
-        ];
-        break;
+// # Записываем сообщение пользователя
+// $message = mb_strtolower(($data['text'] ? $data['text'] : $data['data']),'utf-8');
 
 
-    case 'видео':
-        $method = 'sendVideo';
-        $send_data = [
-            'video'   => 'https://chastoedov.ru/video/amo.mp4',
-            'caption' => 'Вот мое видео',
-            'reply_markup' => [
-                'resize_keyboard' => true,
-                'keyboard' => [
-                    [
-                        ['text' => 'Кнопка 1'],
-                        ['text' => 'Кнопка 2'],
-                    ],
-                    [
-                        ['text' => 'Кнопка 3'],
-                        ['text' => 'Кнопка 4'],
-                    ]
-                ]
-            ]
-        ];
-        break;
+// # Обрабатываем сообщение
+// switch ($message)
+// {
+//     case 'текст':
+//         $method = 'sendMessage';
+//         $send_data = [
+//             'text'   => 'Вот мой ответ'
+//         ];
+//         break;
 
-    default:
-        $method = 'sendMessage';
-        $send_data = [
-            'text' => 'Не понимаю о чем вы :('
-        ];
-}
+//     case 'кнопки':
+//         $method = 'sendMessage';
+//         $send_data = [
+//             'text'   => 'Вот мои кнопки',
+//             'reply_markup' => [
+//                 'resize_keyboard' => true,
+//                 'keyboard' => [
+//                     [
+//                         ['text' => 'Видео'],
+//                         ['text' => 'Кнопка 2'],
+//                     ],
+//                     [
+//                         ['text' => 'Кнопка 3'],
+//                         ['text' => 'Кнопка 4'],
+//                     ]
+//                 ]
+//             ]
+//         ];
+//         break;
 
-# Добавляем данные пользователя
-$send_data['chat_id'] = $data['chat']['id'];
 
-$res = sendTelegram($method, $send_data);
+//     case 'видео':
+//         $method = 'sendVideo';
+//         $send_data = [
+//             'video'   => 'https://chastoedov.ru/video/amo.mp4',
+//             'caption' => 'Вот мое видео',
+//             'reply_markup' => [
+//                 'resize_keyboard' => true,
+//                 'keyboard' => [
+//                     [
+//                         ['text' => 'Кнопка 1'],
+//                         ['text' => 'Кнопка 2'],
+//                     ],
+//                     [
+//                         ['text' => 'Кнопка 3'],
+//                         ['text' => 'Кнопка 4'],
+//                     ]
+//                 ]
+//             ]
+//         ];
+//         break;
 
-function sendTelegram($method, $data, $headers = [])
-{
-    $curl = curl_init();
-    curl_setopt_array($curl, [
-        CURLOPT_POST => 1,
-        CURLOPT_HEADER => 0,
-        CURLOPT_RETURNTRANSFER => 1,
-        CURLOPT_URL => 'https://api.telegram.org/bot' . TOKEN . '/' . $method,
-        CURLOPT_POSTFIELDS => json_encode($data),
-        CURLOPT_HTTPHEADER => array_merge(array("Content-Type: application/json"), $headers)
-    ]);   
+//     default:
+//         $method = 'sendMessage';
+//         $send_data = [
+//             'text' => 'Не понимаю о чем вы :('
+//         ];
+// }
+
+// # Добавляем данные пользователя
+// $send_data['chat_id'] = $data['chat']['id'];
+
+// $res = sendTelegram($method, $send_data);
+
+// function sendTelegram($method, $data, $headers = [])
+// {
+//     $curl = curl_init();
+//     curl_setopt_array($curl, [
+//         CURLOPT_POST => 1,
+//         CURLOPT_HEADER => 0,
+//         CURLOPT_RETURNTRANSFER => 1,
+//         CURLOPT_URL => 'https://api.telegram.org/bot' . TOKEN . '/' . $method,
+//         CURLOPT_POSTFIELDS => json_encode($data),
+//         CURLOPT_HTTPHEADER => array_merge(array("Content-Type: application/json"), $headers)
+//     ]);   
     
-    $result = curl_exec($curl);
-    curl_close($curl);
-    return (json_decode($result, 1) ? json_decode($result, 1) : $result);
-}
+//     $result = curl_exec($curl);
+//     curl_close($curl);
+//     return (json_decode($result, 1) ? json_decode($result, 1) : $result);
+// }
 
-?>
+// ?>
