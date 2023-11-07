ff5f301652888ebb3fa2cd069261d87a82247e60
snoz-an <snoz.andrey94@mail.ru>
2023-11-07 20:27:55 +0300

    fix

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ $data = json_decode(file_get_contents('php://input'), TRUE);
 //file_put_contents('file.txt', '$data: '.print_r($data, 1)."\n", FILE_APPEND);
 
 
-//https://api.telegram.org/bot6915062776:AAEDGQ4EJ56SKgjlrvvKR_P665gVunHpWww/setwebhook?url=*ссылка на бота*
+//https://api.telegram.org/bot6915062776:AAEDGQ4EJ56SKgjlrvvKR_P665gVunHpWww/setwebhook?url=https://github.com/snoz-an/mybot/index.php
 
 
 # Обрабатываем ручной ввод или нажатие на кнопку
